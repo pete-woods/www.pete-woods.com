6439ad85fc63fb3ed3b8fae31ebe38c8978a7357
Pete Woods <email@pete-woods.com>
2017-04-20 13:29:34 +0100

    Build script now cleans the public dir and sets cache control headers

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -euo pipefail
-hugo
+hugo --cleanDestinationDir
 gsutil -m rsync -d -r -x '\..*' public gs://www.pete-woods.com
+gsutil -m setmeta -h "Cache-Control:private, max-age=0, no-transform" -r gs://www.pete-woods.com/
