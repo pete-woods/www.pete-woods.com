f424782dd2d0c7d00f067ebd91efc8a7f3cfd53f
Pete Woods <email@pete-woods.com>
2017-04-20 13:45:51 +0100

    Set cache control to public

--- build.sh
+++ build.sh
@@ -2,4 +2,9 @@
 set -euo pipefail
 hugo --cleanDestinationDir
 gsutil -m rsync -d -r -x '\..*' public gs://www.pete-woods.com
-gsutil -m setmeta -h "Cache-Control:private, max-age=0, no-transform" -r gs://www.pete-woods.com/
+
+CACHE_CONTROL="Cache-Control:public, max-age=300"
+#CACHE_CONTROL="Cache-Control:private, max-age=0, no-transform"
+
+gsutil -m setmeta -h "$CACHE_CONTROL" -r gs://www.pete-woods.com/
+
