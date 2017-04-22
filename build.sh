b8d955c68de91541e0150814d0f0a543b2499cb1
Pete Woods <email@pete-woods.com>
2017-04-22 08:12:48 +0100

    Redirect whole domain to pete-woods.com

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 # Config
-BUCKET="gs://www.pete-woods.com"
+BUCKET="gs://pete-woods.com"
 CACHE_CONTROL="Cache-Control:public, max-age=300"
 #CACHE_CONTROL="Cache-Control:private, max-age=0, no-transform"
 
@@ -18,3 +18,9 @@ for var in images img downloads
 	do
 	gsutil -m setmeta -h "Cache-Control" -r "$BUCKET/$var"
 done
+
+# Set up rediect site
+rsync -r -d static/downloads redirect/
+rsync -d static/img/tesla-factory.gif redirect/img/
+gsutil -m rsync -d -r -x '\..*|.*\/\..*' redirect "gs://www.pete-woods.com"
+gsutil -m setmeta -h "$CACHE_CONTROL" -r "gs://www.pete-woods.com"
