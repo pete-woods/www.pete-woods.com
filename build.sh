061d8591ad8d5eef7c1fd79c531fb7ea3cce28dd
Pete Woods <email@pete-woods.com>
2017-04-22 07:27:57 +0100

    Improve cache control settings

--- build.sh
+++ build.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 set -euo pipefail
-hugo --cleanDestinationDir
-gsutil -m rsync -d -r -x '\..*' public gs://www.pete-woods.com
 
+# Config
+BUCKET="gs://www.pete-woods.com"
 CACHE_CONTROL="Cache-Control:public, max-age=300"
 #CACHE_CONTROL="Cache-Control:private, max-age=0, no-transform"
 
-gsutil -m setmeta -h "$CACHE_CONTROL" -r gs://www.pete-woods.com/
+# Build the site
+hugo --cleanDestinationDir
 
+# Sync the files
+gsutil -m rsync -d -r -x '\..*|.*\/\..*' public "$BUCKET"
+
+# Set cache control headers
+gsutil -m setmeta -h "$CACHE_CONTROL" -r "$BUCKET"
+for var in images img downloads
+	do
+	gsutil -m setmeta -h "Cache-Control" -r "$BUCKET/$var"
+done
