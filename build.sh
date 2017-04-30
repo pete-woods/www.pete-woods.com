276b86a9aeffb02ea598b00afc6fc5509a3c23f8
Pete Woods <email@pete-woods.com>
2017-04-30 09:06:44 +0100

    Switch to host using Google firebase

--- build.sh
+++ build.sh
@@ -1,26 +1,9 @@
 #!/bin/bash
 set -euo pipefail
 
-# Config
-BUCKET="gs://pete-woods.com"
-CACHE_CONTROL="Cache-Control:public, max-age=300"
-#CACHE_CONTROL="Cache-Control:private, max-age=0, no-transform"
-
 # Build the site
 hugo --cleanDestinationDir
 
-# Sync the files
-gsutil -m rsync -d -r -x '\..*|.*\/\..*' public "$BUCKET"
+# Deploy the site
+firebase deploy
 
-# Set cache control headers
-gsutil -m setmeta -h "$CACHE_CONTROL" -r "$BUCKET"
-for var in images img downloads
-	do
-	gsutil -m setmeta -h "Cache-Control" -r "$BUCKET/$var"
-done
-
-# Set up rediect site
-rsync -r -d static/downloads redirect/
-rsync -d static/img/tesla-factory.gif redirect/img/
-gsutil -m rsync -d -r -x '\..*|.*\/\..*' redirect "gs://www.pete-woods.com"
-gsutil -m setmeta -h "$CACHE_CONTROL" -r "gs://www.pete-woods.com"
